9b407783264e3d3b7fd1d20c28a59f10ab6b4978
mikelin <mikelin@mozat.com>
2014-08-30 13:59:43 +0800

    fix handlebar templete bugs

--- src/js/view/app.js
+++ src/js/view/app.js
@@ -71,22 +71,12 @@ define([
         },
 
         registerHandlebarsPlugins : function() {
-            Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-                switch (operator) {
-                    case '==':
-                        return (v1 === v2) ? options.fn(this) : options.inverse(this);
-                    case '<':
-                        return (v1 < v2) ? options.fn(this) : options.inverse(this);
-                    case '<=':
-                        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-                    case '>':
-                        return (v1 > v2) ? options.fn(this) : options.inverse(this);
-                    case '>=':
-                        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-                    default:
-                        return options.inverse(this);
-                }
-            });
+//            Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+//                if(v1 === v2) {
+//                    return options.fn(this);
+//                }
+//                return options.inverse(this);
+//            });
         },
 
         // Add a single todo item to the list by creating a view for it, and
--- src/js/view/todo.js
+++ src/js/view/todo.js
@@ -6,8 +6,9 @@ define([
     'underscore',
     'backbone',
     'text!tmpl/item.handlebars',
+    'handlebars',
     'common'
-], function ($, _, Backbone, todosTemplate, Common) {
+], function ($, _, Backbone, todosTemplate, Handlebars, Common) {
     'use strict';
 
     var TodoView = Backbone.View.extend({
@@ -26,16 +27,14 @@ define([
             'blur .edit':		'close'
         },
 
-        // The TodoView listens for changes to its model, re-rendering. Since there's
-        // a one-to-one correspondence between a **Todo** and a **TodoView** in this
-        // app, we set a direct reference on the model for convenience.
         initialize: function () {
             this.listenTo(this.model, 'change', this.render);
             this.listenTo(this.model, 'destroy', this.remove);
             this.listenTo(this.model, 'visible', this.toggleVisible);
+
+            this.registerHandlebarsPlugins();
         },
 
-        // Re-render the titles of the todo item.
         render: function () {
             this.$el.html(this.template(this.model.toJSON()));
             this.$el.toggleClass('completed', this.model.get('completed'));
@@ -43,6 +42,15 @@ define([
             this.toggleVisible();
             this.$input = this.$('.edit');
             return this;
+        },
+
+        registerHandlebarsPlugins : function() {
+//            Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+//                if(v1 === v2) {
+//                    return options.fn(this);
+//                }
+//                return options.inverse(this);
+//            });
         },
 
         toggleVisible: function () {
@@ -57,18 +65,15 @@ define([
                 );
         },
 
-        // Toggle the `"completed"` state of the model.
         toggleCompleted: function () {
             this.model.toggle();
         },
 
-        // Switch this view into `"editing"` mode, displaying the input field.
         edit: function () {
             this.$el.addClass('editing');
             this.$input.focus();
         },
 
-        // Close the `"editing"` mode, saving changes to the todo.
         close: function () {
             var value = this.$input.val();
             var trimmedValue = value.trim();
@@ -77,9 +82,6 @@ define([
                 this.model.save({ title: trimmedValue });
 
                 if (value !== trimmedValue) {
-                    // Model values changes consisting of whitespaces only are not causing change to be triggered
-                    // Therefore we've to compare untrimmed version with a trimmed one to chech whether anything changed
-                    // And if yes, we've to trigger change event ourselves
                     this.model.trigger('change');
                 }
             } else {
@@ -89,24 +91,19 @@ define([
             this.$el.removeClass('editing');
         },
 
-        // If you hit `enter`, we're through editing the item.
         updateOnEnter: function (e) {
             if (e.keyCode === Common.ENTER_KEY) {
                 this.close();
             }
         },
 
-        // If you're pressing `escape` we revert your change by simply leaving
-        // the `editing` state.
         revertOnEscape: function (e) {
             if (e.which === Common.ESCAPE_KEY) {
                 this.$el.removeClass('editing');
-                // Also reset the hidden input back to the original value.
                 this.$input.val(this.model.get('title'));
             }
         },
 
-        // Remove the item, destroy the model from *localStorage* and delete its view.
         clear: function () {
             this.model.destroy();
         }
