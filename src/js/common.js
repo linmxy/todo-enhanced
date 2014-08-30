fcc85cbc38695c3ca9a2842db101f4519861a0ad
mikelin <mikelin@mozat.com>
2014-08-30 11:39:04 +0800

    add handlebar to requirejs config, fix requirejs

---
+++ src/js/common.js
@@ -0,0 +1,10 @@
+'use strict';
+
+define([], function () {
+    return {
+        TodoFilter: '', // empty, active, completed
+
+        ENTER_KEY: 13,
+        ESCAPE_KEY: 27
+    };
+});
--- src/js/main.js
+++ src/js/main.js
@@ -2,44 +2,79 @@
  * Created by linmin on 29/8/14.
  */
 require.config({
-//    shim: {
-//        underscore: {
-//            exports: '_'
-//        },
-//        backbone: {
-//            deps: [
-//                'underscore',
-//                'jquery'
-//            ],
-//            exports: 'Backbone'
-//        },
+    shim: {
+        JQuery: {
+            exports: '$'
+        },
+        Underscore: {
+            exports: '_'
+        },
+        Handlebars: {
+            exports: 'Handlebars'
+        },
+        Backbone: {
+            deps: [
+                'Underscore',
+                'JQuery'
+            ],
+            exports: 'Backbone'
+        },
 //        backboneLocalstorage: {
 //            deps: ['backbone'],
 //            exports: 'storage'
 //        }
-//    },
+    },
     paths: {
-        $: [
+        JQuery: [
             'http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
             'lib/jquery/jquery'
         ],
-        _: [
+        Underscore: [
             'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
             'lib/underscore/underscore'
         ],
         Backbone: [
-            'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js',
+            'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
             'lib/backbone/backbone'
         ],
         Storage: [
-            'http://cdnjs.cloudflare.com/ajax/libs/backbone-localstorage.js/1.1.13/backbone.localStorage-min.js',
+            'http://cdnjs.cloudflare.com/ajax/libs/backbone-localstorage.js/1.1.13/backbone.localStorage-min',
             'lib/backbone.Localstorage/backbone.Localstorage'
         ],
         Text: [
-            'http://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min.js',
+            'http://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
             'lib/requirejs-text/text'
+        ],
+        Handlebars: [
+//            'http://cdnjs.cloudflare.com/ajax/libs/requirejs-handlebars/0.0.2/hbars.min',
+            'lib/handlebars/handlebars'
         ]
     }
-}).require([], function () {
-    console.log("it works!");
+});
+
+require(['Backbone','Handlebars'], function (Backbone, Handlebars) {
+    Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+        switch (operator) {
+            case '==':
+                return (v1 === v2) ? options.fn(this) : options.inverse(this);
+            case '===':
+                return (v1 === v2) ? options.fn(this) : options.inverse(this);
+            case '<':
+                return (v1 < v2) ? options.fn(this) : options.inverse(this);
+            case '<=':
+                return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+            case '>':
+                return (v1 > v2) ? options.fn(this) : options.inverse(this);
+            case '>=':
+                return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+            case '&&':
+                return (v1 && v2) ? options.fn(this) : options.inverse(this);
+            case '||':
+                return (v1 || v2) ? options.fn(this) : options.inverse(this);
+            default:
+                return options.inverse(this);
+        }
+    });
+    console.log("it works!", Backbone);
+
 });
